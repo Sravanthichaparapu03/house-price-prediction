d6c37e870aebd517566c9fd1c113a2317674d1b5
Sravanthichaparapu03 <sravanthichaparapu99@gmail.com>
2026-04-05 23:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load model
+# Load model and scaler
 model = pickle.load(open('model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 st.title("🏠 California House Price Prediction")
-
-st.write("Enter the details:")
 
 MedInc = st.number_input("Median Income")
 HouseAge = st.number_input("House Age")
@@ -20,6 +19,9 @@ Longitude = st.number_input("Longitude")
 
 if st.button("Predict"):
     features = np.array([[MedInc, HouseAge, AveRooms, AveBedrms, Population, AveOccup, Latitude, Longitude]])
-    prediction = model.predict(features)
+    
+    scaled_features = scaler.transform(features)  # 🔥 important
+    
+    prediction = model.predict(scaled_features)
     
     st.success(f"Predicted House Price: {prediction[0]}")
